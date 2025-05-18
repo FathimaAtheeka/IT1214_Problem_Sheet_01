5d43859221caa24e943a3fed780e20c7ec2b892e
FathimaAtheeka <fathimaatheeka135@gmail.com>
2025-05-18 11:36:16 +0000

    done

--- 01.java
+++ 01.java
@@ -3,7 +3,7 @@ class Student{
     private String name;
     private int daysAttended;
 
-    Student(int studentId, String name, int daysAttended){
+    public Student(int studentId, String name, int daysAttended){
         this.studentId=studentId;
         this.name=name;
         this.daysAttended=daysAttended;
@@ -18,7 +18,7 @@ class Student{
     public String getName(){
         return name;
     }
-    public void setName(String Name){
+    public void setName(String name){
         this.name=name;
     }
     public int getDaysAttended(){
@@ -27,9 +27,62 @@ class Student{
     public void setDaysAttended(int daysAttended){
         this.daysAttended=daysAttended;
     }
-    
+    public String displayDetails(){
+        return "Student Id: " + studentId + ", Name: " + name + ", Days Attended: " + daysAttended;
+    }
+}
 
+class Classroom{
+    private Student[] students;
+    private int studentCount;
+
+    public Classroom(){
+        students = new Student[10];
+        studentCount = 0;
+    }
+
+    public void addStudent(Student student){
+        if(studentCount < students.length){
+            students[studentCount] = student;
+            studentCount++;
+        }
+        else{
+            System.out.println("Classroom is full.Cannot add more students.");
+        }
+    }
+    public void updateDaysAttended(int studentId, int newDaysAttended){
+        for (int i =0; i < studentCount; i++){
+            if (students[i].getStudentId() == studentId){
+                students[i].setDaysAttended(newDaysAttended);
+                return;//stop further execution of the method
+            }
+            
+        }
+        System.out.println("Student with ID "+ studentId + "does not exist.");
+    }
+    public void displayAllStudents(){
+        for (int i=0; i < studentCount; i++){
+            System.out.println(students[i].displayDetails());
+        }
+    }
+
+}
+
+class StudentAttendenceManagement{
     public static void main(String[] args){
-        
+        Classroom classroom = new Classroom();
+
+        Student student1 = new Student(101, "Alice Smith", 12);
+        Student student2 = new Student(102, "Bob Jones", 15);
+        Student student3 = new Student(103, "Carol Lee", 10);
+
+        classroom.addStudent(student1);
+        classroom.addStudent(student2);
+        classroom.addStudent(student3);
+
+
+        classroom.updateDaysAttended(102,16);
+        classroom.updateDaysAttended(104,8);
+        classroom.displayAllStudents();
     }
 }
--- 02.java
+++ 02.java
@@ -1,9 +1,96 @@
 class Vehicle{
     private String licensePlate;
     private String ownerName;
-    private Int hoursParked;
+    private int hoursParked;
 
-    public static void main (String[] args){
-        
+    public Vehicle(String licensePlate, String ownerName, int hoursParked){
+        this.licensePlate = licensePlate;
+        this.ownerName = ownerName;
+        this.hoursParked = hoursParked;
+    }
+
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+    public void setLicensePlate(String licensePlate){
+        this.licensePlate = licensePlate;
+    }
+    public String getOwnerName(){
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName){
+        this.ownerName = ownerName;
+    }
+     public int getHoursParked(){
+        return hoursParked;
+    }
+    public void setHoursParked(int hoursParked){
+        this.hoursParked = hoursParked;
+    }
+
+    public String displayDetails(){
+        return "License plate: " + licensePlate + ", Owner: " + ownerName + ", Hours Parked: " + hoursParked;
+    }
+
+}
+
+class ParkingLot {
+    private Vehicle[] vehicles;
+    private int vehicleCount;
+
+    public ParkingLot(){
+        vehicles = new Vehicle[5];
+        vehicleCount = 0;
+    }
+
+    public void parkVehicle(Vehicle vehicle){
+        if(vehicleCount < vehicles.length){
+            vehicles[vehicleCount] = vehicle;
+            vehicleCount++;
+            System.out.println("Vehicle parked: " + vehicle.displayDetails());
+        }
+        else{
+            System.out.println("Parking lot is full. cannot park more vehicles.");
+        }
+    }
+    public void removeVehicle(String licensePlate){
+        for (int i = 0; i < vehicleCount; i++){
+            if (vehicles[i].getLicensePlate().equals(licensePlate)){
+                for(int j = i; j < vehicleCount - 1; j++){
+                    vehicles[j] = vehicles[j + 1];
+                }
+                vehicles[vehicleCount - 1] = null;
+                vehicleCount--;
+                System.out.println("Vehicle Removed: " + licensePlate);
+                return;
+            }
+        }
+        System.out.println("Vehicle with license plate " + licensePlate + " not found.");
+    }
+    public void displayAllVehicles(){
+        if (vehicleCount == 0){
+            System.out.println("No vehicles parked.");
+            return;
+        }
+        for(int i = 0; i < vehicleCount; i++){
+            System.out.println(vehicles[i].displayDetails());
+        }
+    }
+}
+
+class ParkingLotVehicleTracker{
+        public static void main (String[] args){
+        ParkingLot parkingLot = new ParkingLot();
+
+        Vehicle vehicle1 = new Vehicle("ABC123", "John Doe", 2);
+        Vehicle vehicle2 = new Vehicle("XYZ789", "Jane Smith", 4);
+        Vehicle vehicle3 = new Vehicle("LMN456", "Bob Brown", 1);
+
+        parkingLot.parkVehicle(vehicle1);
+        parkingLot.parkVehicle(vehicle2);
+        parkingLot.parkVehicle(vehicle3);
+
+        parkingLot.removeVehicle("XYZ789");
+        parkingLot.displayAllVehicles();
     }
 }
