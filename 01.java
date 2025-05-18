06cc604fed139a664557dca27e62fddbb1a2a4b8
FathimaAtheeka <fathimaatheeka135@gmail.com>
2025-05-18 06:33:00 +0000

    done

--- 01.java
+++ 01.java
@@ -3,6 +3,32 @@ class Student{
     private String name;
     private int daysAttended;
 
+    Student(int studentId, String name, int daysAttended){
+        this.studentId=studentId;
+        this.name=name;
+        this.daysAttended=daysAttended;
+    }
+
+    public int getStudentId(){
+        return studentId;
+    }
+    public void setStudentId(int studentId){
+        this.studentId=studentId;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String Name){
+        this.name=name;
+    }
+    public int getDaysAttended(){
+        return daysAttended;
+    }
+    public void setDaysAttended(int daysAttended){
+        this.daysAttended=daysAttended;
+    }
+    
+
     public static void main(String[] args){
         
     }
